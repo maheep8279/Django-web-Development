3123712697257b43bbbca1524c4b70d4bee05e12
CHAUHAN <maheep.chauhan@capgemini.com>
2023-10-16 19:17:06 +0530

    Final Commit

--- blog/views.py
+++ blog/views.py
@@ -37,7 +37,9 @@ class SinglePostView(View):
         context = {
             "post":post,
             "post_tags":post.tags.all(),
-            "comment_form":CommentForm()
+            "comment_form":CommentForm(),
+            "comments":post.comments.all().order_by("-id")
+
         }
         return render(request,"blog/post-detail.html",context)
 
@@ -60,7 +62,8 @@ class SinglePostView(View):
         context = {
             "post":post,
             "post_tags":post.tags.all(),
-            "comment_form":CommentForm()
+            "comment_form":CommentForm(),
+            "comments":post.comments.all().order_by("-id")
         }
         return render(request,"blog/post-detail.html",context)
 
